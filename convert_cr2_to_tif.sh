566398a9015645882cedf14b8b28db7143e84c5d
andre_tofa <rfh717@alumni.ku.dk>
2019-03-16 16:19:17 +0100

    added dest_path to conversion of cr2 to tif script

--- convert_cr2_to_tif.sh
+++ convert_cr2_to_tif.sh
@@ -2,9 +2,10 @@
 
 # creates tif file for all cr2 files in a folder
 
-#usage: ./convert_cr2_to_tif.sh path_to_folder/
+#usage: ./convert_cr2_to_tif.sh path_to_folder/ path_to_save_folder/
 
 folder="$1"
+out_path="$2"
 
 # processes raw files
 for f in $folder*.CR2;
@@ -12,6 +13,6 @@ do
         echo "Processing $f"
         ufraw-batch \
                 --out-type=tif \
-                --out-path=. \
+                --out-path=$out_path \
                 $f
 done;
